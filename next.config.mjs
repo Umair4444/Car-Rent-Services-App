db3a5f67c6ae612ceef4f22f8f407f6df191144a
Umair Ali Khan <100639296+Umair4444@users.noreply.github.com>
2025-09-13 11:04:27 +0500

    Car Rent Service App Design

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+// next.config.mjs
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
